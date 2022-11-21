7690acc31e6d007318794e7a2e0ef2243c759e7b
Andgc <91562138+Andgc@users.noreply.github.com>
2022-11-21 12:26:09 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,43 @@
+$("#menu").click(function () {
+  $("#cont_menu").slideToggle();
+});
+
+//SLIDE DE FOTOS CON FLECHA//
+$(function () {
+  $("#slider4").responsiveSlides({
+    auto: false,
+    pager: false,
+    nav: true,
+    speed: 500,
+    namespace: "callbacks",
+    before: function () {
+      $('.events').append("<li>before event fired.</li>");
+    },
+    after: function () {
+      $('.events').append("<li>after event fired.</li>");
+    }
+  });
+});
+// Funcion para desplazamiento de los vinculos
+$(".btn_ancla").click(function () {
+  var ancla = $(this).attr("href");
+  $("html,body").animate(
+    {
+      scrollTop: $(ancla).offset().top,
+    },
+    1000
+  );
+  $("#contenedormenu").toggleClass("abrirmenu");
+});
+
+// Funcion para el boton subir
+$(document).ready(function () {
+  $("#subir").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: "0px",
+      },
+      1000
+    );
+  });
+});
